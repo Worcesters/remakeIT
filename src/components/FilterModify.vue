b84d0f99eb016824ccb8ccb6b827fbe9130cdd74
TheoBIET <dev.theobiet@gmail.com>
2022-03-31 13:24:51 +0200

    ➕ | Add new filters

--- src/components/FilterModify.vue
+++ src/components/FilterModify.vue
@@ -29,16 +29,72 @@ export default {
           name: " Teintes - Vieille photo",
         },
         {
-          code: "hsv",
-          name: "Teinte - Saturation - Valeur",
+          code: "invert",
+          name: "Négatif",
         },
         {
-          code: "rgb",
-          name: "Rouge - Vert - Bleu",
+          code: "solarize",
+          name: "Solarisation",
         },
         {
-          code: "default",
-          name: "Aucun filtre",
+          code: "4bit",
+          name: "4 bits",
+        },
+        {
+          code: "8bit",
+          name: "8 bits",
+        },
+        {
+          code: "mirror",
+          name: "Mode mirroir",
+        },
+        {
+          code: "boxBlur",
+          name: "Flou",
+        },
+        {
+          code: "gaussianBlur",
+          name: "Flou gaussien",
+        },
+        {
+          code: "unsharpMask",
+          name: "Masque de flou",
+        },
+        {
+          code: "sharpen",
+          name: "Netteté",
+        },
+        {
+          code: "contour",
+          name: "Contour",
+        },
+        {
+          code: "detail",
+          name: "Détails",
+        },
+        {
+          code: "edgeEnhance",
+          name: "Effet de bord",
+        },
+        {
+          code: "edgeEnhanceMore",
+          name: "(plus) d'Effets de bord",
+        },
+        {
+          code: "emboss",
+          name: "Embosser",
+        },
+        {
+          code: "findEdges",
+          name: "Trouver les bords",
+        },
+        {
+          code: "smooth",
+          name: "Lissage",
+        },
+        {
+          code: "smoothMore",
+          name: "(plus) de lissage",
         },
       ],
     };
@@ -59,17 +115,20 @@ export default {
 
 <style lang="scss">
 #filterModify {
+  height: 100%;
   .filters {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    max-height: 35em;
+    overflow: auto;
+    padding: 1rem;
 
     .filter {
       padding: 1.5rem 3em;
       background-color: #f5f5f5;
       border-radius: 0.5em;
       cursor: pointer;
-      transition: all 0.2s ease-in-out;
       box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;
       font-weight: 800;
 
@@ -80,11 +139,19 @@ export default {
       }
 
       &.active {
-        background: linear-gradient(45deg,
-                var(--base) 25%,
-                var(--complimentary2));  
+        background: linear-gradient(
+          45deg,
+          var(--base) 25%,
+          var(--complimentary2)
+        );
         color: #fff;
         border: 3px solid #f5f5f5;
+
+        &:hover {
+          transform: translateY(0);
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+            rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+        }
       }
     }
   }
