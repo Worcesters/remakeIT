5b4894e45679e35b67cebfdbb6531f300bffecd7
TheoBIET <dev.theobiet@gmail.com>
2022-04-01 14:15:38 +0200

    🔧 | Fix contain

--- src/views/UpdateView.vue
+++ src/views/UpdateView.vue
@@ -272,7 +272,7 @@ body {
               img {
                 height: 100%;
                 width: 100%;
-                object-fit: cover;
+                object-fit: contain;
               }
             }
           }
